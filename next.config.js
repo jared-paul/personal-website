f9d759dcafc7cd8946a2e79ead7582cb0c26e683
jared-paul <jaredpaul9820@gmail.com>
2021-01-06 20:13:01 -0800

    reimported config

--- next.config.js
+++ next.config.js
@@ -2,3 +2,8 @@ const withMDX = require("@next/mdx")({
     extension: /\.mdx?$/
 });
   
+module.exports = withMDX({
+    pageExtensions: ["js", "jsx", "md", "mdx"],
+    basePath: '/personal-website',
+    assetPrefix: '/personal-website/',
+});
