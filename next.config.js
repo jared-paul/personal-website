a5c8e77d7a2fc3d720981c0281be8c6bf9232347
jared-paul <jaredpaul9820@gmail.com>
2021-01-06 20:06:09 -0800

    removed svg loader

--- next.config.js
+++ next.config.js
@@ -6,15 +6,4 @@ module.exports = withMDX({
     pageExtensions: ["js", "jsx", "md", "mdx"],
     basePath: '/personal-website',
     assetPrefix: '/personal-website/',
-    webpack(config) {
-        config.module.rules.push({
-          test: /\.svg$/,
-          issuer: {
-            test: /\.(js|ts)x?$/,
-          },
-          use: ['@svgr/webpack'],
-        });
-    
-        return config;
-      },
 });
