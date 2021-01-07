af1bdbbf384f322951e277dd9dfd3bc64f98ed8e
jared-paul <jaredpaul9820@gmail.com>
2021-01-06 20:11:25 -0800

    testing

--- next.config.js
+++ next.config.js
@@ -2,8 +2,3 @@ const withMDX = require("@next/mdx")({
     extension: /\.mdx?$/
 });
   
-module.exports = withMDX({
-    pageExtensions: ["js", "jsx", "md", "mdx"],
-    basePath: '/personal-website',
-    assetPrefix: '/personal-website/',
-});
