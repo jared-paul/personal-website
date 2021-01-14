a6cda9d4fe6e5371cc09cc95f159220c0e52efb6
jared-paul <jaredpaul9820@gmail.com>
2021-01-13 21:43:50 -0800

    .

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { Post } from "../components/blog/Post";
@@ -28,13 +29,27 @@ export default function Home() {
             <div className="right-column">
               <Row className="justify-content-center text-center">
                 <div>
-                  <h1 className="project-title">Projects</h1>
+                  <Link href={"/projects"}>
+                    <a><h1 className="title">Projects</h1></a>
+                  </Link>
                   <video id="project" src="/personal-website/tinykeep_project.mp4" loop={true} muted={true} width="300"/>
                 </div>
               </Row>
-              <Row className="justify-content-center text-center">
-                <div>
-                  <h1 className="project-title">Blog</h1>
+              <Row>
+                <div className="text-center blog-title">
+                  <Link href={"/blog"}>
+                    <a><h1 className="title">Blog</h1></a>
+                  </Link>
+                </div>
+                <div className="recent-blog-post">
+                  <div className="inner-blog-post">
+                    {posts
+                      .sort((a, b) => new Date(a.module.meta.date) - new Date(b.module.meta.date))
+                      .slice(0, 1)
+                      .map((post) => (
+                      <Post key={post.link} post={post} />
+                    ))}
+                  </div>
                 </div>
               </Row>
             </div>
@@ -43,11 +58,20 @@ export default function Home() {
       </Container>
       <style jsx>
         {`
-          .project-title {
+          #project {
+            width: 59%;
+          }
+          .title {
             margin: 2rem auto;
           }
+          .blog-title {
+            min-width: 100%
+          }
           .right-column {
-            margin-right: 10rem;
+            margin-right: 20rem;
+          }
+          .recent-blog-post {
+            margin: 0 auto;
           }
         `}
       </style>
